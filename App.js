08353e1dbd5d518a0d491207172ec3668417f5d6
bhawna2123 <mithibhawana@gmail.com>
2025-05-20 16:42:23 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,107 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import LoginScreen from './assets/screen/login/LoginScreen';
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
+import * as SecureStore from "expo-secure-store";
+import { NavigationContainer } from '@react-navigation/native';
+import TabNavigation from './assets/screen/login/navigation/TabNavigation';
+import * as Location from 'expo-location';
+import React, { useState, useEffect } from 'react';
+import { UserLocationContext } from './assets/screen/context/UserLocationContext';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+SplashScreen.preventAutoHideAsync();
+const tokenCache = {
+  async getToken(key) {
+    try {
+      return SecureStore.getItemAsync(key);
+    } catch (err) {
+      return null;
+    }
+  },
+  async saveToken(key, value) {
+    try {
+      return SecureStore.setItemAsync(key, value);
+    } catch (err) {
+      return;
+    }
+  },
+};
+export default function App() {
+
+  const [fontsLoaded] = useFonts({
+    'Outfit-Regular': require('./assets/fonts/Quicksand-Regular.ttf'),
+    'Outfit-SemiBold': require('./assets/fonts/Quicksand-SemiBold.ttf'),
+    'Outfit-Bold': require('./assets/fonts/Quicksand-Bold.ttf'),
+  });
+
+  const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location.coords);
+      console.log(location)
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+  return (
+
+    <ClerkProvider
+      tokenCache={tokenCache}
+      publishableKey={'pk_test_YnJhdmUtc25pcGUtOTYuY2xlcmsuYWNjb3VudHMuZGV2JA'}>
+      <UserLocationContext.Provider
+        value={{ location, setLocation }}>
+
+
+        <View style={styles.container} onLayout={onLayoutRootView}>
+          <SignedIn>
+            <NavigationContainer>
+              <TabNavigation />
+            </NavigationContainer>
+          </SignedIn>
+          <SignedOut>
+            <LoginScreen />
+          </SignedOut>
+          <StatusBar style="auto" />
+        </View>
+      </UserLocationContext.Provider>
+    </ClerkProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 2
+  },
+});
